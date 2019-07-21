92ae087e1b00cf7f9c2fc8d46777ec411b8d6c1b
Metalblueberry <metalblueberry@gmail.com>
2019-07-21 17:56:51 +0000

    improve run script

--- run_game.sh
+++ run_game.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 #go build MyBot.go
-docker exec -u 1000 -it --workdir "/home/victor/Halite-bot/"  halite-bot_devcontainer_development_1 go build
-halite -d "240 160" "./cmd/MyBot/MyBot" "./cmd/MyBot/MyBot"
-find -type f -name "replay*" | sort | tail -n 1 | xargs -I{} chlorine -o {}
+docker exec -u 1000 -it --workdir "/home/victor/Halite-bot/cmd/MyBot"  halite-bot_devcontainer_development_1 go build && \
+halite -d "240 160" "./cmd/MyBot/MyBot" "./cmd/MyBot/MyBot" && \
+find -type f -name "replay*" | sort | tail -n 1 | xargs -I{} chlorine -o {} && \
+rm replay* && \
+rm *.log
