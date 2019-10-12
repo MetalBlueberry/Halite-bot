202fe8bfc8ffc29cc09cf9ed90709ffa6baa42e3
Metalblueberry <metalblueberry@gmail.com>
2019-10-12 19:23:33 +0200

    improve scripts

--- run_game.sh
+++ run_game.sh
@@ -2,7 +2,7 @@
 
 rm logs/* 
 #go build MyBot.go
-halite -t -d "240 160" "go run ./cmd/stdinToWebsocket/main.go" "go run ./cmd/MyBot/*.go --debug=false --server=false --logToFile" && \
+halite -t -d "240 160" "go run ./cmd/stdinToWebsocket/main.go" "docker run --rm -i unity:v0.1.0" && \
 find -type f -name "replay*" | grep -v "save" | sort | tail -n 1 | xargs -I{} chlorine -o {} && \
 rm replay* && \
 rm *.log
--- run_local.sh
+++ run_local.sh
@@ -2,7 +2,8 @@
 
 rm logs/* 
 #go build MyBot.go
-halite -t -d "240 160" "go run ./cmd/MyBot/*.go --server=FALSE --logToFile=TRUE" "/home/victor/Documents/Projects/Halite-II/airesources/Python3/MyBot.py" && \
+#halite -t -d "240 160" "go run ./cmd/MyBot/*.go --server=FALSE --logToFile=TRUE" "/home/victor/Documents/Projects/Halite-II/airesources/Python3/MyBot.py" && \
+halite -t -d "240 160" "docker run --rm -i unity" "docker run --rm -i unity"  && \
 find -type f -name "replay*" | grep -v "save" | sort | tail -n 1 | xargs -I{} chlorine -o {} && \
 rm replay* && \
 rm *.log
