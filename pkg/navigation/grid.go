98de8969e4c1dff5d5e5a7769ba8a9224e5c3ef6
Metalblueberry <metalblueberry@gmail.com>
2019-10-12 17:38:52 +0200

    reduce ship size in grid

--- pkg/navigation/grid.go
+++ pkg/navigation/grid.go
@@ -32,7 +32,7 @@ func NewGrid(Width, Height int) *Grid {
 
 func (g *Grid) PaintShip(X float64, Y float64, shotRange float64) {
 	g.Paint(X, Y, shotRange, ShotRange)
-	g.Paint(X, Y, 1.5, Ship)
+	g.Paint(X, Y, 1.0, Ship)
 }
 
 func (g *Grid) PaintPlanet(X float64, Y float64, radius float64) {
