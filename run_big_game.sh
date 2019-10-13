dde91d0c372e4f0fa25a5142a70225895deb45e0
Metalblueberry <metalblueberry@gmail.com>
2019-10-13 21:07:28 +0200

    script to run 4 player games

---
+++ run_big_game.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm logs/* 
+#go build MyBot.go
+halite -t -d "240 160" \
+    "go run ./cmd/stdinToWebsocket/main.go" \
+    "docker run --rm -i unity:v0.2.0" \
+    "docker run --rm -i unity:v0.2.0" \
+    "docker run --rm -i unity:v0.1.0" && \
+find -type f -name "replay*" | grep -v "save" | sort | tail -n 1 | xargs -I{} chlorine -o {} && \
+rm replay* && \
+rm *.log
