ea145fa5974ca7ff915f4b52e2dd36803b8fd916
Metalblueberry <metalblueberry@gmail.com>
2019-11-02 21:14:21 +0100

    comment out tests

--- pkg/control/commander_test.go
+++ pkg/control/commander_test.go
@@ -2,9 +2,8 @@ package control_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-
-	. "github.com/metalblueberry/halite-bot/pkg/control"
+	//. "github.com/onsi/gomega"
+	//. "github.com/metalblueberry/halite-bot/pkg/control"
 )
 
 var _ = Describe("Test Planet Stats", func() {
